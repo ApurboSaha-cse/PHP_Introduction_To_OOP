414c7f6a8232e7568a871fdecebbedeb74c0f119
ApurboSaha-cse <apurbosaha.cse@gmail.com>
2023-10-15 22:28:08 +0600

    Implement the methods on Book and Customer classes

--- Book.php
+++ Book.php
@@ -1,45 +1,66 @@
 <?php
 
-class Book {
+class Book
+{
     private int $isbn;
     private string $title;
     private string $author;
     private int $available;
 
-    public function getIsbn(): int {
-        return $this->isbn;
-    }
-    
-    public function getTitle(): string {
-        return $this ->title;
-    }
-
-    public function getAuthor(): string {
-        return $this->author;
-    }
-
-    public function getAvailable(): int {
-        return $this->available;
-    }
-
-    public function setIsbn(string $isbn) {
+    //constructor
+    public function __construct(int $isbn, string $title, string $author, int $available)
+    {
         $this->isbn = $isbn;
-    }
-
-    public function setTitle(string $title) {
         $this->title = $title;
-    }
-
-    public function setAuthor(string $author) {
         $this->author = $author;
-    }
-
-    public function setAvailable(int $available) {
         $this->available = $available;
     }
 
+    //The getter methods
+    public function getIsbn(): int
+    {
+        return $this->isbn;
+    }
 
-    public function getCopy(): bool {
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function getAvailable(): int
+    {
+        return $this->available;
+    }
+
+    //The setter methods
+    public function setIsbn(string $isbn)
+    {
+        $this->isbn = $isbn;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function setAuthor(string $author)
+    {
+        $this->author = $author;
+    }
+
+    public function setAvailable(int $available)
+    {
+        $this->available = $available;
+    }
+
+    //getCopy() function
+    public function getCopy(): bool
+    {
         if ($this->available > 0) {
             $this->available = $this->available - 1;
             return true;
@@ -48,8 +69,46 @@ class Book {
         }
     }
 
-    public function addCopy($num): int{
-        $this ->available = $this ->available + $num;
-        return $this ->available;
+    //addCopy() function
+    public function addCopy($num): int
+    {
+        $this->available = $this->available + $num;
+        return $this->available;
+    }
+
+    //Implement the method : __toString
+    public function __toString()
+    {
+        $details =
+            "</br>ISBN: " . $this->isbn .
+            "</br>Title: " . $this->title .
+            "</br>Author: " . $this->author .
+            "</br>Available: " . $this ->available;
+
+        return $details;
+    }
+
+    // Implement the method: __call()
+    public function __call($method, $value)
+    {
+        return "This method doesn't exits.";
+    }
+
+    // Implement the method: __get()
+    public function __get($var_name)
+    {
+        if (property_exists(__CLASS__, $var_name)) {
+            return $this->{$var_name};
+        }
+        return "\This property doesn't exist.";
+    }
+
+    // Implement the method: __set()
+    public function __set($var_name, $value)
+    {
+        if (property_exists(__CLASS__, $var_name)) {
+            $this->{$var_name} = $value;
+        }
+        return "\nThis property doesn't exist.";
     }
 }
--- Customer.php
+++ Customer.php
@@ -6,6 +6,15 @@ class Customer{
     private string $lastName;
     private string $email;
 
+    // Constructor
+    public function __construct(int $id, string $firstName, string $lastName, string $email){
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }
+
+    //The getter methods
     public function getId(): int{
         return $this->id;
     }
@@ -22,6 +31,7 @@ class Customer{
         return $this->email;
     }
 
+    //The setter methods
     public function setId($id){
         $this->id = $id;
     }
@@ -37,4 +47,37 @@ class Customer{
     public function setEmail($email){
         $this->email = $email;
     }
+
+    // Implement the magic method: __toString()
+    public function __toString(){
+        $details=
+        "</br>ID: ".$this->id.
+        "</br>First Name: ".$this->firstName.
+        "</br>Last Name: ".$this->lastName.
+        "</br>Email: ".$this ->email;
+
+        return $details;
+    }
+
+    // Implement the method: __call()
+    public function __call($method, $value){
+        return "This method doesn't exits.";
+    }
+
+    // Implement the method: __get()
+    public function __get($var_name){
+        if(property_exists(__CLASS__, $var_name)){
+            return $this->{$var_name};
+        }
+        return "\nThis property doesn't exist.";
+    }
+
+    // Implement the method: __set()
+    public function __set($var_name, $value){
+        if(property_exists(__CLASS__, $var_name)){
+            $this->{$var_name} = $value;
+        }
+        return "\nThis property doesn't exist.";
+    }
 }
+
